0dc7f4f4c7f49a9685c7d88f677f22c054e77f6a
Christian Axel <axeltjahjadi@yahoo.com>
2015-12-25 01:14:55 -0500

    4 thumbnails working

--- Permias Michigan Website/Website Directory/script.js
+++ Permias Michigan Website/Website Directory/script.js
@@ -1,20 +1,60 @@
-//<script  type="text/javascript">
-function() {
-	var isFirst = true;
+var isFirst1 = true;
+var isFirst2 = true;
+var isFirst3 = true;
+var isFirst4 = true;
 
-$('.round_thumb_container img:gt(0)').hide();
-$(".round_thumb_container").hover(function(){
-	if (isFirst) {    
-    timer = setInterval(function(){   $('.round_thumb_container :first-child').fadeOut()
+$('.round_thumb_container1 img:gt(0)').hide();
+$(".round_thumb_container1").hover(function(){
+	if (isFirst1) {    
+    timer = setInterval(function(){   $('.round_thumb_container1 :first-child').fadeOut()
     .next('img').fadeIn()
-    .end().appendTo('.round_thumb_container');},             
-    750)
-    isFirst = false;
+    .end().appendTo('.round_thumb_container1');},             
+    500);
+    isFirst1 = false;
   }
 }, function() {
 clearInterval(timer);
-isFirst = true;
+isFirst1 = true;
 });
-}
-          
-//        </script>
+
+$('.round_thumb_container2 img:gt(0)').hide();
+$(".round_thumb_container2").hover(function(){
+	if (isFirst2) {    
+    timer = setInterval(function(){   $('.round_thumb_container2 :first-child').fadeOut()
+    .next('img').fadeIn()
+    .end().appendTo('.round_thumb_container2');},             
+    500);
+    isFirst2 = false;
+  }
+}, function() {
+clearInterval(timer);
+isFirst2 = true;
+});
+
+$('.round_thumb_container3 img:gt(0)').hide();
+$(".round_thumb_container3").hover(function(){
+	if (isFirst3) {    
+    timer = setInterval(function(){   $('.round_thumb_container3 :first-child').fadeOut()
+    .next('img').fadeIn()
+    .end().appendTo('.round_thumb_container3');},             
+    500);
+    isFirst3 = false;
+  }
+}, function() {
+clearInterval(timer);
+isFirst3 = true;
+});
+
+$('.round_thumb_container4 img:gt(0)').hide();
+$(".round_thumb_container4").hover(function(){
+	if (isFirst4) {    
+    timer = setInterval(function(){   $('.round_thumb_container4 :first-child').fadeOut()
+    .next('img').fadeIn()
+    .end().appendTo('.round_thumb_container4');},             
+    500);
+    isFirst4 = false;
+  }
+}, function() {
+clearInterval(timer);
+isFirst4 = true;
+});
