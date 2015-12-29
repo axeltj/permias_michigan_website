457dca62c48a298eed7486bec9bb7d0c5eb1019e
Christian Axel <axeltjahjadi@yahoo.com>
2015-12-29 08:19:56 -0500

    Entire Gallery's structure complete.
    Need to add pictures

---
+++ Permias Michigan Website/Website Directory/gallery-script.js
@@ -0,0 +1,28 @@
+$(function() { // document ready
+
+	if (!!$('.event_description_container').offset()) { // make sure .event_description_container exists
+
+		// TODO: if adding event descriptions, add ID to IDs and div's Top position to Tops
+		// IDs and Tops must have the same length.
+		var IDs = ["#ed1",
+					"#ed2",
+					"#ed3"];
+		var Tops = [$('#ed1').offset().top,
+					$('#ed2').offset().top,
+					$('#ed3').offset().top];
+
+		$(document).scroll( function () { // when scroll
+			var windowTop = $(document).scrollTop(); // update window's top
+			// update all event_description_containers
+			for (i = 0; i < IDs.length; i++) {
+				if (Tops[i] < windowTop) { // windowTop is below div's top position
+					$(IDs[i]).css({ position: 'fixed', width: '16%' });
+				}
+				else {
+					$(IDs[i]).css({ position: 'static', width: '20%'});
+				}
+			}
+		});
+	}
+
+})
