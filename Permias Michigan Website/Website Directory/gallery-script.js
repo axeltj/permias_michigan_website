9bff68e9b063f369852ff4a51d573151d7a12ff4
Christian Axel <axeltjahjadi@yahoo.com>
2015-12-30 00:56:13 -0500

    Thumbnails now fully working
    Need to substitute photos with real event photos and add description.
    Also need batik background image for event_container

--- Permias Michigan Website/Website Directory/gallery-script.js
+++ Permias Michigan Website/Website Directory/gallery-script.js
@@ -26,10 +26,21 @@ $(function() { // document ready
 		});
 	}
 
-	/* THUMBNAIL PREVIEW*/
-	$("#thumb_picture_container").click(function(){
-    	$(this).addClass("large");
-        $("#overlay").removeClass("hideoverlay").addClass("viewoverlay");
+	/* THUMBNAIL CLICK TO VIEW FULL PHOTO */
+	$(".thumb_picture_container").click(function() {
+		$("#overlay").removeClass("hideoverlay").addClass("viewoverlay"); // show overlay
+		$(this).children('img').attr( 'src', $(this).children('img').attr('data-large-img-src') ); // set new img src
+		$(this).attr('id','large'); // switch to large img
+	});
+
+	/* CLOSE FULL PHOTO VIEW */
+	$("#overlay").click(function() {
+		$(".thumb_picture_container").attr('id','small'); // switch to small img
+		// $(".thumb_picture_container").children('img').attr( 'src', $(this).attr('data-small-img-src') ); // ALTERNATIVE TO FUNCTION CALL BELOW
+		$("#overlay").removeClass("viewoverlay").addClass("hideoverlay");
+		$(".thumb_picture_container img").each(function() { // Makes ALL thumbnails use small img src
+			$(this).attr( 'src', $(this).attr('data-small-img-src') );
+		});
 	});
 
 
