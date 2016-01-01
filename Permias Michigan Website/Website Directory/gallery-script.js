680cf6836ec79449c3d6f9474b1f13e34ef55b38
Christian Axel <axeltjahjadi@yahoo.com>
2015-12-31 21:20:00 -0500

    ALLL WORKINGGG

--- Permias Michigan Website/Website Directory/gallery-script.js
+++ Permias Michigan Website/Website Directory/gallery-script.js
@@ -3,24 +3,28 @@ $(function() { // document ready
 	/* EVENT DESCRIPTION SCROLL */
 	if (!!$('.event_description_container').offset()) { // make sure .event_description_container exists
 
-		// TODO: if adding event descriptions, add ID to IDs and div's Top position to Tops
+		// TODO: if adding events (and therefore also event descriptions), add ID to IDs array and div's Top position to Tops array
 		// IDs and Tops must have the same length.
 		var IDs = ["#ed1",
 					"#ed2",
-					"#ed3"];
+					"#ed3",
+					"#ed4",
+					"#ed5"];
 		var Tops = [$('#ed1').offset().top,
 					$('#ed2').offset().top,
-					$('#ed3').offset().top];
+					$('#ed3').offset().top,
+					$('#ed4').offset().top,
+					$('#ed5').offset().top];
 
 		$(document).scroll( function () { // when scroll
 			var windowTop = $(document).scrollTop(); // update window's top
 			// update all event_description_containers
 			for (i = 0; i < IDs.length; i++) {
 				if (Tops[i] < windowTop) { // windowTop is below div's top position
-					$(IDs[i]).css({ position: 'fixed', width: '16%', height: '100px', left: '10%', top: '45px' });
+					$(IDs[i]).css({ position: 'fixed', width: '16%', left: '10%', top: '45px' });
 				}
 				else {
-					$(IDs[i]).css({ position: 'absolute', width: '20%', height: '100%', left: '0', top: '0' });
+					$(IDs[i]).css({ position: 'absolute', width: '20%', left: '0', top: '0' });
 				}
 			}
 		});
